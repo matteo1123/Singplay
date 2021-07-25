ddd35bce5a178afa0d8f7f2a91147b3d0dd205d9
Matthew Semroska <matthew.semroska@gmail.com>
2021-07-25 03:05:02 -0600

    cicd test

--- template-parts/page/subscribe.php
+++ template-parts/page/subscribe.php
@@ -4,4 +4,5 @@
     let image = "<?php echo the_post_thumbnail_url(); ?>"
     if(!image) image = window.location.origin + '/wp-content/uploads/2021/07/cropped-music-images-6-scaled-4.jpg' ;
     let title = "Subscribe for premium content!";
+    console.log('hello')
 </script>
